7c8ffcf2e07200f95b72b568ec881a493c5600e3
pavish <pavishkumar.r@gmail.com>
2020-04-10 23:34:09 +0530

    Show mixed content error message for codesandbox viewers

--- src/App.js
+++ src/App.js
@@ -4,6 +4,23 @@ import Status from "./Status";
 import Table from './Table';
 import "./styles.css";
 
+const API_ENDPOINT = "http://starlord.hackerearth.com/TopSellingGames";
+
+function ErrorMessage() {
+  return (
+    <div className="error">
+      <h3>Error in fetching data</h3>
+      <h4>
+        This may occur if you're running the app on codesandbox due to Mixed Content error. <br/><br/>
+        The API at {API_ENDPOINT} is served using http only, while codesandbox is served in https. <br/><br/>
+        Newer browsers block this behaviour by default. Older versions will allow this by enabling Insecure content for the site. <br/><br/>
+
+        Please download the repo and run this locally to view the app: <a href="https://github.com/pavish/top-selling-games-react">https://github.com/pavish/top-selling-games-react</a>
+      </h4>
+    </div>
+  )
+}
+
 export default class App extends React.Component {
   constructor(props) {
     super(props);
@@ -14,7 +31,7 @@ export default class App extends React.Component {
   }
 
   componentDidMount() {
-    fetch("http://starlord.hackerearth.com/TopSellingGames")
+    fetch(API_ENDPOINT)
       .then(response => response.json())
       .then(data => {
         this.setState({
@@ -36,9 +53,9 @@ export default class App extends React.Component {
         <h1>
           Top Selling Games of all time
         </h1>
-        {this.state.status === Status.FETCHING && <Spinner />}
+        {this.state.status === Status.FETCHING && <Spinner/>}
         {this.state.status === Status.FETCHED && <Table gameRankList={this.state.gameRankList}/>}
-        {this.state.status === Status.ERROR && <h3 className="error">Error in fetching data</h3>}
+        {this.state.status === Status.ERROR && <ErrorMessage/>}
       </div>
     );
   }
